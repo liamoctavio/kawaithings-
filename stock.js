ee39911fe89082f2a63b7d53c163338873f8f995
liamoctavio <ocmolina47@gmail.com>
2022-02-18 21:50:38 -0300

    Actualizacion

---
+++ stock.js
@@ -0,0 +1,7 @@
+let stockProductos = [
+    {id:1, nombre: "Perrito",precio: 1200},
+    {id:2, nombre: "Gatito",precio:2500},
+    {id:3, nombre: "Taza Gatito",precio: 2200},
+    {id:4, nombre: "Taza Huron 3",precio: 3200},
+    // {id: 1, nombre: "Buzo 1", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1200, talle: "L", img: '/img/buzo.jpg'}
+]
